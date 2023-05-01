f57976cb250490ec365134e98464d7f2d804ef3a
botan <aginbotan@gmail.com>
2023-05-01 22:35:50 +0300

    Add roxygen header to document geom_col_rounded()

--- R/geom-col-rounded.R
+++ R/geom-col-rounded.R
@@ -1,3 +1,57 @@
+#' Bar plot with rounded corners
+#'
+#' @description This function creates a bar plot with rounded corners using
+#'   ggplot2. It is an extension of the [ggplot2::geom_col()] function and
+#'   provides additional aesthetics for a more visually appealing output.
+#'
+#' @param mapping Set of aesthetic mappings created by [ggplot2::aes()]. If specified and
+#'   `inherit.aes = TRUE` (the default), it is combined with the default mapping
+#'   at the top level of the plot. You must supply `mapping` if there is no plot
+#'   mapping.
+#' @param data The data to be displayed in this layer. There are three
+#'    options:
+#'
+#'    If `NULL`, the default, the data is inherited from the plot
+#'    data as specified in the call to [ggplot2::ggplot()].
+#'
+#'    A `data.frame`, or other object, will override the plot
+#'    data. All objects will be fortified to produce a data frame. See
+#'    [ggplot2::fortify()] for which variables will be created.
+#'
+#'    A `function` will be called with a single argument,
+#'    the plot data. The return value must be a `data.frame`, and
+#'    will be used as the layer data. A `function` can be created
+#'    from a `formula` (e.g. `~ head(.x, 10)`).
+#' @param position Position adjustment, either as a string naming the adjustment
+#'   (e.g. `"jitter"` to use `position_jitter`), or the result of a call to a
+#'   position adjustment function. Use the latter if you need to change the
+#'   settings of the adjustment.
+#' @param radius The radius of the rounded corners, given as a unit object.
+#' @param ... Other arguments passed on to [ggplot2::layer()]. These are
+#'   often aesthetics, used to set an aesthetic to a fixed value, like
+#'   `colour = "red"` or `size = 3`. They may also be parameters
+#'   to the paired geom/stat.
+#' @param width Bar width. If not supplied, the default is to use the width of
+#'   90% of the resolution of the data.
+#' @param na.rm If `FALSE`, the default, missing values are removed with
+#'   a warning. If `TRUE`, missing values are silently removed.
+#' @param show.legend logical. Should this layer be included in the legends?
+#'   `NA`, the default, includes if any aesthetics are mapped.
+#'   `FALSE` never includes, and `TRUE` always includes.
+#'   It can also be a named logical vector to finely select the aesthetics to
+#'   display.
+#' @param inherit.aes If `FALSE`, overrides the default aesthetics,
+#'   rather than combining with them. This is most useful for helper functions
+#'   that define both data and aesthetics and shouldn't inherit behaviour from
+#'   the default plot specification, e.g. [ggplot2::borders()].
+#'
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' ggplot(data.frame(x = letters[1:3], y = c(2.3, 1.9, 3.2)), aes(x, y)) +
+#' geom_col_rounded()
 geom_col_rounded <-
   function(
     mapping = NULL,
