59c6a21807f9416d7c30ad15fcd08b16d5e33d54
botan <aginbotan@gmail.com>
2023-05-06 02:21:50 +0300

    Move vdiffr from Imports to Suggests

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 `%||%` <- function(x, y) if (is.null(x)) y else x # nocov
 
 dummy <- function() { # nocov start
-  vdiffr::expect_doppelganger
+  # vdiffr::expect_doppelganger
   grid::unit
   gridGeometry::polyclipGrob
 } # nocov end
